b03143e8156116828dcda255d5b8fb3b99d3e66a
michalmasir <michal.masir@gmail.com>
2014-10-12 16:09:56 +0200

    doplnenie airport testov a komentarov

---
+++ src/main/java/cz/muni/fi/PA165/flight/dao/AirportDAO.java
@@ -0,0 +1,35 @@
+package cz.muni.fi.PA165.flight.dao;
+
+import cz.muni.fi.PA165.flight.entity.Airport;
+
+import java.util.List;
+
+/**
+ * Created by Michal Mašír on 9. 10. 2014.
+ */
+public interface AirportDAO {
+    /**
+     * Returns the list of all airports in the system
+     * @return list of all airports
+     */
+    public List<Airport> getAllAirports();
+
+    /**
+     * Returns a airport based on its ID
+     * @param id ID of searched airport
+     * @return airport
+     */
+    public Airport getAirportById(long id);
+
+    /**
+     * Deletes an airport
+     * @param airport Airports reference we whant do delete
+     */
+    public void deleteAirport(Airport airport);
+
+    /**
+     * Adds a new airport
+     * @param airport instance of the airport, we want to add
+     */
+    public void addAirport(Airport airport);
+}
