30d54bf6f615b4d23a296fd067e7982d8126c703
misog <miso.galan@gmail.com>
2014-10-12 12:11:44 +0200

    Fix javadoc for Plane

--- src/main/java/cz/muni/fi/PA165/flight/entity/Plane.java
+++ src/main/java/cz/muni/fi/PA165/flight/entity/Plane.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import java.util.Date;
 
 /**
+ * Represents plane in the system.
+ *
  * @author Michal Galan
  */
 @Entity
@@ -40,26 +42,51 @@ public class Plane {
 
     /* METHODS */
 
+    /**
+     * Sets plane manufacturer, ex. "Airbus".
+     * @param manufacturer name of the plane manufacturer
+     */
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
     }
 
+    /**
+     * Sets plane type or model, ex. "320".
+     * @param type type or model of the plane
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * Sets maximum of passanger seats.
+     * @param passangerSeatsCount max passanger seats count in the plane
+     */
     public void setPassangerSeatsCount(int passangerSeatsCount) {
         this.passangerSeatsCount = passangerSeatsCount;
     }
 
+    /**
+     * Sets maximum of staff seats, excluding pilot and navigator seats.
+     * @param staffSeatsCount max seats count for staff, excludung pilot and navigator seats
+     */
     public void setStaffSeatsCount(int staffSeatsCount) {
         this.staffSeatsCount = staffSeatsCount;
     }
 
+    /**
+     * Sets capacity of plane's tank.
+     * @param tankCapacity capacity of the plane tank
+     */
     public void setTankCapacity(int tankCapacity) {
         this.tankCapacity = tankCapacity;
     }
 
+    /**
+     * Sets fuel present in a plane.
+     * @param fuelLeft fuel amount present in the plane
+     * @throws Exception if fuelLeft is more than getTankCapacity().
+     */
     public void setFuelLeft(int fuelLeft) throws Exception {
         if (fuelLeft > getTankCapacity()){
             throw new Exception("Can not tank more than capacity");
@@ -67,6 +94,11 @@ public class Plane {
         this.fuelLeft = fuelLeft;
     }
 
+    /**
+     * Add fuel to plane's tank.
+     * @param fuel fuel amount to tank to the plane
+     * @throws Exception if total fuel would be more than getTankCapacity().
+     */
     public void tankFuel(int fuel) throws Exception {
         if (fuelLeft+fuel > getTankCapacity()){
             throw new Exception("Total fuel can not be more than tank capacity");
@@ -74,42 +106,82 @@ public class Plane {
         this.fuelLeft += fuel;
     }
 
+    /**
+     * Sets the date of last plane revision.
+     * @param lastRevisionTime Date of the last revision
+     */
     public void setLastRevisionTime(Date lastRevisionTime) {
         this.lastRevisionTime = lastRevisionTime;
     }
 
+    /**
+     * Increases total flight time with specified time.
+     * @param inc time
+     */
     public void increaseTotalFlightTime(long inc) {
         this.totalFlightTime += inc;
     }
 
+    /**
+     * Increases total flight distance with specified distance.
+     * @param inc distance
+     */
     public void increaseTotalFlightDistance(long inc) {
         this.totalFlightDistance += inc;
     }
 
+    /**
+     * Returns plane ID.
+     * @return
+     */
     public long getId() {
         return this.id;
     }
 
+    /**
+     * Returns plane manufacturer name.
+     * @return
+     */
     public String getManufacturer() {
         return manufacturer;
     }
 
+    /**
+     * Returns plane type or model.
+     * @return
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Returns plane passanger seats count.
+     * @return
+     */
     public int getPassangerSeatsCount() {
         return passangerSeatsCount;
     }
 
+    /**
+     * Returns plane staff count, excluding pilot and navigator seats.
+     * @return
+     */
     public int getStaffSeatsCount() {
         return staffSeatsCount;
     }
 
+    /**
+     * Returns capacity of plane tank.
+     * @return
+     */
     public int getTankCapacity() {
         return tankCapacity;
     }
 
+    /**
+     * Returns fuel present in a plane.
+     * @return
+     */
     public int getFuelLeft() {
         return fuelLeft;
     }
