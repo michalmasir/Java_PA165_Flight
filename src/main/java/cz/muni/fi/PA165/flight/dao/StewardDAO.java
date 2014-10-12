8cbeb87a625d73796d030fca832ef0520d888b04
michal hruby <mchl.hruby@gmail.com>
2014-10-12 18:12:50 +0200

    Add Steward
    Added Steward with DAO and test

---
+++ src/main/java/cz/muni/fi/PA165/flight/dao/StewardDAO.java
@@ -0,0 +1,48 @@
+package cz.muni.fi.PA165.flight.dao;
+
+import cz.muni.fi.PA165.flight.entity.Steward;
+import java.util.List;
+
+    /**
+     * This interfaces specifies the methods that a {@link cz.muni.fi.PA165.flight.entity.Steward} data access object
+     * must implement.
+     *
+     * @author  Michal Hruby
+     */
+
+    public interface StewardDAO {
+
+        /**
+         * Returns list of all stewards.
+         *
+         * @return list of all stewards
+         */
+        public List<Steward> getAllStewards();
+
+        /**
+         * Gets Steward with given id.
+         *
+         * @param id id of wanted Steward
+         * @return wanted Steward or null if the Steward does not exist
+         */
+
+        public Steward getStewardById(long id );
+
+        /**
+         * Deletes given Steward.
+         *
+         * @param steward Steward to be deleted
+         */
+
+        public void deleteSteward(Steward steward);
+
+        /**
+         * Saves given Steward.
+         *
+         * @param steward Steward to be saved
+         */
+
+        public void addSteward(Steward steward);
+    }
+
+
--- src/main/java/cz/muni/fi/PA165/flight/entity/Steward.java
+++ src/main/java/cz/muni/fi/PA165/flight/entity/Steward.java
@@ -4,10 +4,62 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * @author  Michal Hruby
+ */
+
 @Entity
 public class Steward {
 
     @Id
     @GeneratedValue
     private long id;
+
+    private String first_name;
+
+    private String last_name;
+
+    /**
+     *
+     * @param first_name
+     */
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @param id
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
 }
