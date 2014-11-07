ae866002a78b893c79e3a05338e25af848191b43
michal hruby <mchl.hruby@gmail.com>
2014-11-07 17:25:22 +0100

    Fix StewardTO

--- flight-service/src/main/java/cz/muni/fi/PA165/flight/transfer/StewardTO.java
+++ flight-service/src/main/java/cz/muni/fi/PA165/flight/transfer/StewardTO.java
@@ -49,4 +49,25 @@ public class StewardTO {
     public void setFlights(Set<FlightTO> flights) {
         this.flights = flights;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StewardTO)) return false;
+
+        StewardTO steward = (StewardTO) o;
+
+        if (id != steward.id || id == 0) return false;
+        if (firstName != steward.firstName) return false;
+        if (lastName != steward.lastName) return false;
+        if (flights != null ? !flights.equals(steward.flights) : steward.flights != null) return false;
+
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
 }
